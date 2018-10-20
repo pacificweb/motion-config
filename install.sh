e58675bcca96f02414bdea48de2f9f9e7855c810
stefets <stephane-gagnon@hotmail.com>
2018-10-19 22:46:35 -0400

    Create motion.conf as a symlink

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 target=$HOME/.motion
 mkdir -p $target
+mkdir -p $target/capture
 
 ln -fs $DIR/motion.conf $target/motion.conf 
-ln -fs $DIR/capture $target/capture
